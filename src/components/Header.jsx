88acc394fee4878acc6f7bd57cac3b421316880e
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-05-16 21:33:48 +0300

    Init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://fikiwiki.com/uploads/posts/2022-02/1645039733_10-fikiwiki-com-p-kartinki-logotipov-10.jpg'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://mirpozitiva.ru/wp-content/uploads/2019/11/1476889932_zakat-derevo.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
