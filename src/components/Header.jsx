ccab16702f08a55bec86a81d6321bf4117aecc25
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-05-16 23:05:04 +0300

    css-modules added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import classes from "./Style/Header.module.css";
+
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img
                 src='https://fikiwiki.com/uploads/posts/2022-02/1645039733_10-fikiwiki-com-p-kartinki-logotipov-10.jpg'/>
         </header>
