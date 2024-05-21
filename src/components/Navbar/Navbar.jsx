f4c2959f9956e1d98069a0859341ec8cf522db25
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-05-22 00:42:54 +0300

    react-router-dom added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a>Profile</a>
+                <a href='/Profile'>Profile</a>
             </div>
             <div className={`${classes.item} ${classes.active}`}>
-                <a>Messages</a>
+                <a href='/Dialogs'>Messages</a>
             </div>
             <div className={classes.item}>
                 <a>News</a>
