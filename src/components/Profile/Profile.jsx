d491b0431e8a348885afbc5af9ea5d8ef44533f4
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-05-21 22:46:26 +0300

    Page Dialogs added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,14 +3,15 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
-                <img src='https://mirpozitiva.ru/wp-content/uploads/2019/11/1476889932_zakat-derevo.jpg'/>
+                <img className={classes.image}
+                     src='https://mirpozitiva.ru/wp-content/uploads/2019/11/1476889932_zakat-derevo.jpg'/>
             </div>
             <div>
                 ava + description
             </div>
-            <MyPosts />
+            <MyPosts/>
         </div>
     );
 };
