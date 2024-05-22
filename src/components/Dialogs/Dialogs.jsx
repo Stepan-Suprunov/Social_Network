21a52d62482fa9925b81a4c6460781b95e5c0f1d
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-05-22 16:40:38 +0300

    Dialogs and Messages added

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,35 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dialog
+                </div>
+                <div className={classes.dialog}>
+                    Dialog
+                </div>
+                <div className={classes.dialog}>
+                    Dialog
+                </div>
+                <div className={classes.dialog}>
+                    Dialog
+                </div>
+                <div className={classes.dialog}>
+                    Dialog
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.mesage}>
+                    Hi!
+                </div>
+                <div className={classes.mesage}>
+                    How are you?
+                </div>
+                <div className={classes.mesage}>
+                    Hello World!
+                </div>
+            </div>
         </div>
     );
 };
