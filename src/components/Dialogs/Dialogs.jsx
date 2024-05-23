7b30cd002e141557e7eed61302ef26506d39ea09
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-05-23 13:39:36 +0300

    map method added to arrays

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -13,15 +13,15 @@ const DialogItem = (props) => {
 
 const Message = (props) => {
     return (
-        <div className={classes.mesage}>
+        <div className={classes.message}>
             {props.text}
         </div>
     );
 };
 
-const Dialogs = (props) => {
+const Dialogs = () => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Name1'},
         {id: 2, name: 'Name2'},
         {id: 3, name: 'Name3'},
@@ -29,25 +29,23 @@ const Dialogs = (props) => {
         {id: 5, name: 'Name5'}
     ];
 
-    let mesagesData = [
+    let dialogsElements = dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+
+    let messages = [
         {id: 1, text: 'Hi!'},
         {id: 2, text: 'How are you?'},
         {id: 3, text: "It's my first message!"}
     ];
 
+    let messagesElements = messages.map(message => <Message text={message.text}/>);
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                {dialogsElements}
             </div>
             <div className={classes.messages}>
-                <Message text={mesagesData[0].text}/>
-                <Message text={mesagesData[1].text}/>
-                <Message text={mesagesData[2].text}/>
+                {messagesElements}
             </div>
         </div>
     );
