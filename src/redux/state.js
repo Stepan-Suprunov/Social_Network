e6e6847b961822002e77a3d29dbbab7651c8f97d
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-06-01 01:14:45 +0300

    Inputs location moved to state

--- src/redux/state.js
+++ src/redux/state.js
@@ -5,7 +5,8 @@ let state = {
         posts: [
             {id:1, text:'Hi! How are you?', likes:15},
             {id:2, text:'It\'s my first message!', likes:25}
-        ]
+        ],
+        newPostText: ''
     },
     messagesPage: {
         dialogs: [
@@ -19,17 +20,39 @@ let state = {
             {id: 1, text: 'Hi!'},
             {id: 2, text: 'How are you?'},
             {id: 3, text: "It's my first message!"}
-        ]
+        ],
+        newMessageText: ''
     }
 };
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
     let newPost = {
-        id: 3,
-        text: postMessage,
+        id: state.profilePage.posts.length + 1,
+        text: state.profilePage.newPostText,
         likes: 0
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+};
+
+export let updateNewPostText = (newPostText) => {
+    state.profilePage.newPostText = newPostText;
+    rerenderEntireTree(state);
+};
+
+export let addMessage = () => {
+    let newMessage = {
+        id: state.messagesPage.messages.length + 1,
+        text: state.messagesPage.newMessageText
+    };
+    state.messagesPage.messages.push(newMessage);
+    state.messagesPage.newMessageText = '';
+    rerenderEntireTree(state);
+};
+
+export let updateNewMessageText = (newMessageText) => {
+    state.messagesPage.newMessageText = newMessageText;
     rerenderEntireTree(state);
 };
 
