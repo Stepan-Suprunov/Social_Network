9ed1dc3727d37aa521a94e1e34ce98f2e7481c1e
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-06-11 23:28:08 +0300

    Users.jsx refactored: button "Get Users" added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,57 +2,61 @@ import UserItem from "./UserItem/UserItem";
 import axios from "axios";
 
 let Users = (props) => {
-    if (props.users.length === 0) {
+    const getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
-        });
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items);
+            });
 
-        // props.setUsers([
-        //     {
-        //         id: 1,
-        //         photos: {
-        //                small: null,
-        //                large: null
-        //         },
-        //         followed: true,
-        //         name: 'User 1',
-        //         status: 'Status',
-        //         location: {country: 'Country', city: 'City'},
-        //         uniqueUrlName: null
-        //     },
-        //     {
-        //         id: 2,
-        //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
-        //         followed: true,
-        //         fullName: 'User 2',
-        //         status: 'Status',
-        //         location: {country: 'Country', city: 'City'}
-        //     },
-        //     {
-        //         id: 3,
-        //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
-        //         followed: false,
-        //         fullName: 'User 3',
-        //         status: 'Status',
-        //         location: {country: 'Country', city: 'City'}
-        //     },
-        //     {
-        //         id: 4,
-        //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
-        //         followed: false,
-        //         fullName: 'User 4',
-        //         status: 'Status',
-        //         location: {country: 'Country', city: 'City'}
-        //     }
-        // ])
+            // props.setUsers([
+            //     {
+            //         id: 1,
+            //         photos: {
+            //                small: null,
+            //                large: null
+            //         },
+            //         followed: true,
+            //         name: 'User 1',
+            //         status: 'Status',
+            //         location: {country: 'Country', city: 'City'},
+            //         uniqueUrlName: null
+            //     },
+            //     {
+            //         id: 2,
+            //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
+            //         followed: true,
+            //         fullName: 'User 2',
+            //         status: 'Status',
+            //         location: {country: 'Country', city: 'City'}
+            //     },
+            //     {
+            //         id: 3,
+            //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
+            //         followed: false,
+            //         fullName: 'User 3',
+            //         status: 'Status',
+            //         location: {country: 'Country', city: 'City'}
+            //     },
+            //     {
+            //         id: 4,
+            //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
+            //         followed: false,
+            //         fullName: 'User 4',
+            //         status: 'Status',
+            //         location: {country: 'Country', city: 'City'}
+            //     }
+            // ])
+        }
+        ;
     };
 
     let usersElements = props.users.map(user => <UserItem user={user} modifyFollow={props.modifyFollow}/>);
 
     return (
         <div>
-            {usersElements}
+            <div><button onClick={getUsers}>Get Users</button></div>
+            <div>{usersElements}</div>
         </div>
     );
 };
