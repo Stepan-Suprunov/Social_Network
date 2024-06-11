c05e86b34e9dd69dd7e618bdbbe245fbf149c94c
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-06-12 01:34:36 +0300

    Users refactored to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,63 +1,70 @@
-import UserItem from "./UserItem/UserItem";
+import React from 'react';
+import userPhotoDefault from "../../assets/images/user_avatar_default.png";
+import classes from "./Users.module.css";
 import axios from "axios";
 
-let Users = (props) => {
-    const getUsers = () => {
-        if (props.users.length === 0) {
+class Users extends React.Component {
 
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items);
-            });
+    //         id: 1,
+    //         photos: {
+    //                small: null,
+    //                large: null
+    //         },
+    //         followed: true,
+    //         name: 'User 1',
+    //         status: 'Status',
+    //         location: {country: 'Country', city: 'City'},
+    //         uniqueUrlName: null
 
-            // props.setUsers([
-            //     {
-            //         id: 1,
-            //         photos: {
-            //                small: null,
-            //                large: null
-            //         },
-            //         followed: true,
-            //         name: 'User 1',
-            //         status: 'Status',
-            //         location: {country: 'Country', city: 'City'},
-            //         uniqueUrlName: null
-            //     },
-            //     {
-            //         id: 2,
-            //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
-            //         followed: true,
-            //         fullName: 'User 2',
-            //         status: 'Status',
-            //         location: {country: 'Country', city: 'City'}
-            //     },
-            //     {
-            //         id: 3,
-            //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
-            //         followed: false,
-            //         fullName: 'User 3',
-            //         status: 'Status',
-            //         location: {country: 'Country', city: 'City'}
-            //     },
-            //     {
-            //         id: 4,
-            //         avatarUrl: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png',
-            //         followed: false,
-            //         fullName: 'User 4',
-            //         status: 'Status',
-            //         location: {country: 'Country', city: 'City'}
-            //     }
-            // ])
-        }
-        ;
+    constructor(props) {
+        super(props);
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
+        });
     };
 
-    let usersElements = props.users.map(user => <UserItem user={user} modifyFollow={props.modifyFollow}/>);
+    render ()  {
 
-    return (
-        <div>
-            <div><button onClick={getUsers}>Get Users</button></div>
-            <div>{usersElements}</div>
+        const followSwitch = (user) => {
+            let follow = '';
+            user.followed ? follow = 'Unfollow' : follow = 'Follow';
+            return follow;
+        }
+
+        return <div>
+            {
+                this.props.users.map(user =>
+                    <div key={user.id}>
+                        <span>
+                            <div>
+                                <img
+                                    src={user.photos.small != null ? user.photos.small : userPhotoDefault}
+                                    alt="Avatar"
+                                    className={classes.userAvatar}
+                                />
+                            </div>
+                            <div>
+                                <button onClick={() => {
+                                    this.props.modifyFollow(user.id)
+                                }}>
+                                    {followSwitch(user)}
+                                </button>
+                            </div>
+                        </span>
+                        <span>
+                            <div>{user.name}</div>
+                            <div>{user.status}</div>
+                        </span>
+                        <span>
+                            <div>{'props.user.location.city'}</div>
+                            <div>{'props.user.location.country'}</div>
+                        </span>
+                    </div>
+                )
+            };
         </div>
-    );
+    };
 };
+
 export default Users;
