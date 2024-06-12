130e683ca8a1d0261abbed9ce360fb7cba2737e3
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-06-12 17:07:44 +0300

    Users page pagination added

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,21 +1,30 @@
 import {connect} from "react-redux";
 import Users from "./Users";
-import {modifyFollowAC, setUsersAC} from "../../redux/usersReducer";
+import {modifyFollowAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC} from "../../redux/usersReducer";
 
 
-let mapStateToProps = (state) => {
+const mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     };
 };
 
-let mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
     return {
         modifyFollow: (userId) => {
             dispatch(modifyFollowAC(userId));
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (currentPage) => {
+            dispatch(setCurrentPageAC(currentPage));
+        },
+        setTotalUsersCount: (usersCount) => {
+            dispatch(setTotalUsersCountAC(usersCount));
         }
     };
 };
